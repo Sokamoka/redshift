6f6494bf72d07e9fe2eb8a04d99f06312d059b22
Akos Stegner <a.stegner@mito.hu>
2023-02-15 08:37:03 +0100

    Add nitro preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
+  nitro: {
+    preset: "netlify",
+  },
+
   modules: [
     "@nuxtjs/tailwindcss",
     "@vueuse/nuxt",
