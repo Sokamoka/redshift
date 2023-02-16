ed6a22a19bc44195ac7c0ffb489e7040cf28216c
Akos Stegner <a.stegner@mito.hu>
2023-02-16 09:47:04 +0100

    Develop input component

--- components/form/Input.vue
+++ components/form/Input.vue
@@ -1,6 +1,17 @@
+<script lang="ts">
+export default {
+  name: 'FormInput',
+
+  inheritAttrs: false,
+}
+</script>
+
 <template>
-  <input
-    type="text"
-    class="w-full p-4 border-4 border-gray-900 outline-none rounded-md focus-within:border-orange-500"
-  />
+  <div
+    class="flex items-center bg-white border-4 border-gray-900 focus-within:border-orange-500 rounded-md w-full"
+  >
+    <slot name="prepend" />
+    <input type="text" class="p-3 outline-none flex-1 w-full" v-bind="$attrs" />
+    <slot name="append" />
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,24 @@ definePageMeta({
 
 <template>
   <div class="w-full max-w-5xl">
-    <button
-      type="button"
-      class="px-7 py-5 text-base text-gray-900 font-semibold bg-gray-100 rounded-t-md"
-    >
-      Text to Image
-    </button>
-    <button
-      type="button"
-      class="px-7 py-5 text-base text-orange-500 font-semibold bg-white rounded-t-md"
-    >
-      Image to image
-    </button>
+    <div class="flex">
+      <button
+        type="button"
+        class="px-7 py-5 text-base text-gray-900 font-semibold bg-gray-100 rounded-t-md"
+      >
+        Text to Image
+      </button>
+      <button
+        type="button"
+        class="px-7 py-5 text-base text-orange-500 font-semibold bg-white rounded-t-md"
+      >
+        Image to image
+      </button>
+      <div class="flex-1 flex items-center justify-end">
+        <span class="px-5 text-orange-500 font-bold">areuser</span>
+        <div class="bg-orange-500 rounded-full w-10 h-10"></div>
+      </div>
+    </div>
     <div
       class="grid grid-cols-[40%_1fr] grid-rows-[1fr_64px] gap-7 place-items-stretch w-full bg-gray-100 rounded-md p-8"
     >
@@ -29,7 +35,11 @@ definePageMeta({
           <FormTextarea rows="1" placeholder="Type negative promt..." />
         </fieldset>
         <fieldset>
-          <FormInput placeholder="Seed"></FormInput>
+          <FormInput placeholder="Seed">
+            <template v-slot:append>
+              <a href="#" class="is-link px-3">Randomize</a>
+            </template>
+          </FormInput>
         </fieldset>
         <fieldset>
           <FormRange name="clg-scale" label="CLG Scale"></FormRange>
@@ -52,14 +62,10 @@ definePageMeta({
         </div>
       </div>
       <div>
-        <FormButton is-full-width>
-          Generate
-        </FormButton>
+        <FormButton is-full-width> Generate </FormButton>
       </div>
       <div>
-        <FormButton variant="outlined" is-full-width>
-          Make 3D
-        </FormButton>
+        <FormButton variant="outlined" is-full-width> Make 3D </FormButton>
       </div>
     </div>
   </div>
